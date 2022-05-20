b239d57ca7e77b2762b9e06c227980ea0ae059cc
SK <limsweekiat@gmail.com>
2022-05-20 16:01:05 +0800

    Reduce delete modal padding

--- src/components/dashboard/DeleteModal.vue
+++ src/components/dashboard/DeleteModal.vue
@@ -7,7 +7,7 @@
         </TransitionChild>
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-          <div class="relative inline-block align-bottom rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-12"
+          <div class="relative inline-block align-bottom rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:px-12 sm:pb-8 sm:pt-10"
             :class="store.darkMode ? 'bg-neutral-800 text-neutral-300' : 'bg-white text-neutral-800'">
             <div class="h-2 w-full absolute top-0 left-0" :class="store.darkMode ? 'bg-neutral-600' : 'bg-neutral-200'"></div>
             <XIcon class="h-5 absolute top-6 right-4 cursor-pointer" @click="show=false" />
