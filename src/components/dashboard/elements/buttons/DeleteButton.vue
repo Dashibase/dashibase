de2f8ee978107e0d717d1b54628930518c752e24
SK <limsweekiat@gmail.com>
2022-05-22 00:31:51 +0800

    Fix buttons darkMode bug

--- src/components/dashboard/elements/buttons/DeleteButton.vue
+++ src/components/dashboard/elements/buttons/DeleteButton.vue
@@ -1,12 +1,15 @@
 <template>
-  <Button class="border border-neutral-300 bg-neutral-50 text-neutral-600 hover:text-red-600 hover:border-red-600 disabled:text-neutral-300 disabled:bg-neutral-50 dark:border-neutral-700 dark:text-neutral-200 dark:hover:text-red-400 dark:hover:border-red-400 dark:disabled:text-neutral-600 dark:disabled:border-neutral-700"
-    :disabled="props.disabled" @click="emit('click')">
+  <Button :disabled="props.disabled" class="border border-neutral-300 bg-neutral-50 text-neutral-600 hover:text-red-600 hover:border-red-600 disabled:text-neutral-300 disabled:bg-neutral-50 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:text-red-400 dark:hover:border-red-400 dark:disabled:text-neutral-600 dark:disabled:border-neutral-700"
+    @click="emit('click')">
     <slot />
   </Button>
 </template>
 
 <script setup lang="ts">
 import Button from './Button.vue'
+import { useStore } from '@/utils/store'
+
+const store = useStore()
 
 const props = defineProps({
   disabled: {
--- src/components/dashboard/elements/buttons/PrimaryButton.vue
+++ src/components/dashboard/elements/buttons/PrimaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <Button class="shadow bg-neutral-100 text-neutral-700 hover:bg-green-300 disabled:text-neutral-500 disabled:bg-neutral-700 dark:bg-neutral-700 dark:text-neutral-100 dark:hover:bg-green-600 dark:disabled:bg-white dark:disabled:text-neutral-300 dark:disabled:bg-white"
+  <Button class="shadow bg-neutral-700 text-neutral-100 hover:bg-green-600 disabled:bg-neutral-100 disabled:text-neutral-300 dark:bg-neutral-100 dark:text-neutral-700 dark:hover:bg-green-300 dark:disabled:text-neutral-500 dark:disabled:bg-neutral-700"
     :disabled="props.disabled" @click="emit('click')">
     <slot />
   </Button>
