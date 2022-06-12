82abe7cc6045272e5f0d791c2ec273dcf22c2ea8
SK <limsweekiat@gmail.com>
2022-06-12 11:45:09 +0800

    Fix DAS-109 - Fix broken many-to-many multi-select

--- src/utils/dashboard.ts
+++ src/utils/dashboard.ts
@@ -634,7 +634,8 @@ export function initCrud (page:Page, itemId:string|number='') {
               .delete()
               .match({ [schema.getFkColumns(joinTable, page.table_id)[0]]: item.id })
             if (deleteRequest.error) reject(deleteRequest.error.message)
-            const joinItems = item[`${foreignTable}(${schema.getPrimaryKey(foreignTable)})`]
+            const attributeId = Object.keys(item).find(attr => attr.includes(`${foreignTable}(${schema.getPrimaryKey(foreignTable)})`))
+            const joinItems = item[attributeId as string]
               .map((id:any) => {
                 return {
                   [schema.getFkColumns(joinTable, foreignTable)[0]]: id,
